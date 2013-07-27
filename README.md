6ff98a79afd5b8cf538143b45435beb3add5c13a
Oak Chantosa <o.chantosa@gmail.com>
2013-07-27 00:13:47 -0400

    Add support for Meteor 0.6.4.1

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ meteor-livescript
 A Meteor package for the [LiveScript](http://gkz.github.com/LiveScript/) JavaScript dialect, a more functional take on Coffeescript.
 
 See [10 Reasons to Switch from CoffeeScript to LiveScript](http://gkz.github.com/LiveScript/blog/ten-reasons-to-switch-from-coffeescript.html).
+
+7/27/2013 - Added support for Meteor 0.6.4.1
--- package.js
+++ package.js
@@ -1,27 +1,34 @@
-Package.describe({
-	summary : "Add support for LiveScript, a more functional take on Coffeescript."
+Package.describe({summary: "Add support for LiveScript, a more functional take on Coffeescript."});
+
+Npm.depends({
+  'LiveScript': '1.1.1'
 });
 
-var ls = require('LiveScript');
-var fs = require('fs');
+var livescript_handler = function (bundle, source_path, serve_path, where) {
+  var fs = Npm.require('fs'),
+      path = Npm.require('path'),
+      livescript = Npm.require('LiveScript'),
+      serve_path = serve_path + '.js',
+      contents = fs.readFileSync(source_path);
+  options = {
+    bare: true
+  };
+  try {
+    contents = livescript.compile(contents.toString('utf8'), options);
+  } catch (e) {
+    return bundle.error(
+      source_path + ':' +
+      (e.location ? (e.location.first_line + ': ') : ' ') +
+      e.message
+    );
+  }
+  contents = new Buffer(contents);
+  bundle.add_resource({
+    type: 'js',
+    path: serve_path,
+    data: contents,
+    where: where
+  });
+};
 
-Package.register_extension("ls", function(bundle, source_path, serve_path, where) {
-	serve_path = serve_path + '.js';
-
-	var contents = fs.readFileSync(source_path);
-	var options = {
-		bare : true
-	};
-	contents = new Buffer(ls.compile(contents.toString('utf8'), options));
-
-	bundle.add_resource({
-		type : "js",
-		path : serve_path,
-		data : contents,
-		where : where
-	});
-});
-
-Package.on_test(function(api) {
-	api.add_files(['livescript_tests.ls', 'livescript_tests.js'], ['client', 'server']);
-}); 
+Package.register_extension('ls', livescript_handler);
