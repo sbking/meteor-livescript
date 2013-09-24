c1db67e2cfcb96dc0c6ccb3853b38630ee46585b
Stephen King <stephen.brian.king@gmail.com>
2013-09-24 09:33:53 -0700

    Add support for Meteor 0.6.5.1

--- package.js
+++ package.js
@@ -1,34 +1,10 @@
 Package.describe({summary: "Add support for LiveScript, a more functional take on Coffeescript."});
 
-Npm.depends({
-  'LiveScript': '1.1.1'
+Package._transitional_registerBuildPlugin({
+  name: "compileLiveScript",
+  use: [],
+  sources: [
+    'plugin/compile-livescript.js'
+  ],
+  npmDependencies: {"LiveScript": "1.2.0"}
 });
-
-var livescript_handler = function (bundle, source_path, serve_path, where) {
-  var fs = Npm.require('fs'),
-      path = Npm.require('path'),
-      livescript = Npm.require('LiveScript'),
-      serve_path = serve_path + '.js',
-      contents = fs.readFileSync(source_path);
-  options = {
-    bare: true
-  };
-  try {
-    contents = livescript.compile(contents.toString('utf8'), options);
-  } catch (e) {
-    return bundle.error(
-      source_path + ':' +
-      (e.location ? (e.location.first_line + ': ') : ' ') +
-      e.message
-    );
-  }
-  contents = new Buffer(contents);
-  bundle.add_resource({
-    type: 'js',
-    path: serve_path,
-    data: contents,
-    where: where
-  });
-};
-
-Package.register_extension('ls', livescript_handler);
